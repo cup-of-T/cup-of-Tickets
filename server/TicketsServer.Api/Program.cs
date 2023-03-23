9ca07fc5759573e41d8cafe446d72fc8c2944551
cparn <caspar.rodoo@gmail.com>
2023-03-23 18:39:29 +0100

    Add cors policy

--- server/TicketsServer.Api/Program.cs
+++ server/TicketsServer.Api/Program.cs
@@ -21,6 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(cors =>
+   {
+       cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+   });
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
