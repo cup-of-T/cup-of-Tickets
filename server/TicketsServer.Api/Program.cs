b1ba890b4e4b6b9851e29670339653fd044fed54
cparn <caspar.rodoo@gmail.com>
2023-03-23 18:44:26 +0100

    add swagger to production

--- server/TicketsServer.Api/Program.cs
+++ server/TicketsServer.Api/Program.cs
@@ -17,9 +17,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(cors =>
    {
