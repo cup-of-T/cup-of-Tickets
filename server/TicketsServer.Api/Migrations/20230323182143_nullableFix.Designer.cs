5cec0542fcc91fe602c8e46870e0676e9e540a30
Luc-Siat <lucsiat@gmail.com>
2023-03-23 19:47:34 +0100

    removes comments in program.cs

--- server/TicketsServer.Api/Migrations/20230323182143_nullableFix.Designer.cs
+++ server/TicketsServer.Api/Migrations/20230323182143_nullableFix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace TicketsServer.Api.Migrations
 {
     [DbContext(typeof(DbContext))]
-    partial class DbContextModelSnapshot : ModelSnapshot
+    [Migration("20230323182143_nullableFix")]
+    partial class nullableFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
