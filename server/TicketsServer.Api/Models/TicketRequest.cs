4c2350474420de0197e41bb1dea78bd2928b7e20
Caspar Rödöö <caspar.rodoo@appliedtechnology.se>
2023-03-23 22:10:33 +0100

    Add ticket request

--- server/TicketsServer.Api/Models/TicketRequest.cs
+++ server/TicketsServer.Api/Models/TicketRequest.cs
@@ -6,8 +6,6 @@ public class TicketRequest
 {
     public required string Title { get; set; }
     public required string Description { get; set; }
-    public bool Completed { get; set; }
-    public bool Archived { get; set; }
     public int Urgency { get; set; }
     public string? Category { get; set; }
     public string? TimeEstimate { get; set; }
