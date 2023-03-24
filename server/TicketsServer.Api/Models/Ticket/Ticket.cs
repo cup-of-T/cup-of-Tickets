fdbccdd8adeb74594e3bedce097e777202b33bfd
cparn <caspar.rodoo@gmail.com>
2023-03-24 15:54:06 +0100

    added status

--- server/TicketsServer.Api/Models/Ticket/Ticket.cs
+++ server/TicketsServer.Api/Models/Ticket/Ticket.cs
@@ -13,6 +13,7 @@ public class Ticket
     public string? Description { get; set; }
     public bool Archived { get; set; }
     public int Urgency { get; set; }
+    public int Status { get; set; }
     public required string TimeEstimate { get; set; }
     public bool Completed { get; set; }
     public required User Creator { get; set; }
