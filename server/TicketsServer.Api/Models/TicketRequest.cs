e250549b76e52adc53d588ae422b850ce541ea3d
cparn <caspar.rodoo@gmail.com>
2023-03-24 09:28:43 +0100

    fix timeestimate

--- server/TicketsServer.Api/Models/TicketRequest.cs
+++ server/TicketsServer.Api/Models/TicketRequest.cs
@@ -6,7 +6,7 @@ public class TicketRequest
 {
     public required string Title { get; set; }
     public required string Description { get; set; }
+    public required string TimeEstimate { get; set; }
     public int Urgency { get; set; }
-    public string? TimeEstimate { get; set; }
     public required List<string> CategoryNames { get; set; }
 }
