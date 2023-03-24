eaa64639b07c48631327f9fb13ef1ce1d4bcb659
idkwhoiam <donnasoujeh@gmail.com>
2023-03-24 10:20:58 +0100

    implement protected route

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import React, { ComponentType } from "react";
-import { Route, RouteProps } from "react-router-dom";
 
 interface ProtectedRouteProps {
     component: ComponentType,
