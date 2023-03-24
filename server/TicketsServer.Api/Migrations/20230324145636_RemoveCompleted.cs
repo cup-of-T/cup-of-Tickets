1154daf6a9778f983c2baba06fddf9420c85bf82
cparn <caspar.rodoo@gmail.com>
2023-03-24 15:57:04 +0100

    removed completed

---
+++ server/TicketsServer.Api/Migrations/20230324145636_RemoveCompleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketsServer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Tickets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
