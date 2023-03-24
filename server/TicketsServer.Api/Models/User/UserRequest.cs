5cebc8db15c31963e3d6f19ce750f3c666dc0a22
Luc-Siat <lucsiat@gmail.com>
2023-03-24 11:51:51 +0100

    create Post in user

---
+++ server/TicketsServer.Api/Models/User/UserRequest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketsServer.Api.Models;
+
+public class UserRequest
+{
+    public required string Email { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string Role { get; set; }
+}
