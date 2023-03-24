209c7d69417d40155a977ae1cf33090da1e9eda6
Luc-Siat <lucsiat@gmail.com>
2023-03-24 14:59:45 +0100

    implement img to navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, User } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import LoginButton from "./LoginButton";
 import { LogoutButton } from "./LogOutButton";
@@ -8,15 +8,17 @@ interface IHeaderProps {
 }
 
 const Header = ({ }: IHeaderProps) => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, user } = useAuth0();
     return (
         <header>
                 <nav className="navbar">
                     <div className="container navbar__container">
                         <Link to="/">Home</Link>
                         {!isAuthenticated && <LoginButton />}
-                        {isAuthenticated && <LogoutButton />}
-                        <img src="" />
+                        {isAuthenticated && <>
+                        <LogoutButton />
+                        <img className="navbar__avatar" src={user?.picture} />
+                        </>}
                     </div>
                 </nav>
         </header>
