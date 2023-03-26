14a1834003a2409e7f87fd07030aa82d63c13a37
Luc-Siat <lucsiat@gmail.com>
2023-03-26 16:05:11 +0200

    fixed margin issues

--- frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
+++ frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './ticketcard.css'
 
 export const TicketCard = () => {
   return (
-    <ul className='ticket-board__grid ticket-board__card-styling container'>
+    <ul className='ticket-board__grid fw-300 ticket-board__card-styling container'>
     <input className='ticket-board__checkbox' type="checkbox" />
     <li className='ticket-board__id'>#1</li>
     <li className='ticket-board__text'>Site is broken</li>
