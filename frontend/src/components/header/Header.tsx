4da5885abf2b3857fe37195d4bb7e88ccfa7cfcc
Luc-Siat <lucsiat@gmail.com>
2023-03-26 16:33:56 +0200

    fix logo size

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,5 +1,7 @@
 import { useAuth0, User } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import { Navbar } from "../navbar/Navbar";
+import { Sidebar } from "../sidebar/Sidebar";
 import './Header.css'
 
 interface IHeaderProps {
