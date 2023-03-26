979969f36f1319f899141163086cfe5f74f8a58d
Luc-Siat <lucsiat@gmail.com>
2023-03-26 11:25:25 +0200

    fix padding on the icons

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -6,9 +6,9 @@ export const Sidebar = () => {
   return (
     <aside className="sidebar">
       <menu className="sidebar__links">
-        <Link to=""><i className=" sidebar__links-item fa-solid fa-house-chimney"></i></Link>
-        <Link to=""><i className=" sidebar__links-item fa-solid fa-people-group"></i></Link>
-        <Link to=""><i className=" sidebar__links-item fa-solid fa-gear"></i></Link>
+        <Link className="sidebar__links-item"to=""><i className="fa-solid fa-house-chimney"></i></Link>
+        <Link className="sidebar__links-item"to=""><i className="fa-solid fa-people-group"></i></Link>
+        <Link className="sidebar__links-item"to=""><i className="fa-solid fa-gear"></i></Link>
       </menu>
     </aside>
   )
