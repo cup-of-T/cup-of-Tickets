e7a55145d4f2e462939621e2aea1cb27287664f9
cparn <caspar.rodoo@gmail.com>
2023-03-26 15:13:49 +0200

    change accestoken variable name

--- frontend/src/services/ticketApi.ts
+++ frontend/src/services/ticketApi.ts
@@ -1,56 +1,56 @@
 import { ITicket } from "../interfaces/interface";
 
-export const getTickets = async (accesToken: string) => {
+export const getTickets = async (accessToken: string) => {
   const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Tickets`, {
-    headers: { Authorization: `Bearer ${accesToken}` }
+    headers: { Authorization: `Bearer ${accessToken}` }
   })
   return await response.json() as ITicket[];
 }
 
-export const postTicket = async (ticket: ITicket, accesToken: string) => {
+export const postTicket = async (ticket: ITicket, accessToken: string) => {
   const ticketToJson = JSON.stringify(ticket);
   const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Tickets`, {
     method: 'POST',
     body: ticketToJson,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${accesToken}`
+      Authorization: `Bearer ${accessToken}`
     },
   })
   return await response.json() as ITicket;
 }
 
-export const deleteTicket = async (ticketId: number, accesToken: string) => {
+export const deleteTicket = async (ticketId: number, accessToken: string) => {
   await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Tickets/${ticketId}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${accesToken}`
+      Authorization: `Bearer ${accessToken}`
     },
   })
 }
 
-export const updateTicketStatus = async (ticketId: number, status: number, accesToken: string) => {
+export const updateTicketStatus = async (ticketId: number, status: number, accessToken: string) => {
   const statusToJson = JSON.stringify(status);
   const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Tickets/${ticketId}/status`, {
     method: 'PATCH',
     body: statusToJson,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${accesToken}`
+      Authorization: `Bearer ${accessToken}`
     },
   })
   return await response.json() as ITicket[];
 }
 
-export const updateTicketAssignedTo = async (ticketId: number, assigneeId: number, accesToken: string) => {
+export const updateTicketAssignedTo = async (ticketId: number, assigneeId: number, accessToken: string) => {
   const assigneeIdToJson = JSON.stringify(assigneeId);
   const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Tickets/${ticketId}/assignedto`, {
     method: 'PATCH',
     body: assigneeIdToJson,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${accesToken}`
+      Authorization: `Bearer ${accessToken}`
     },
   })
   return await response.json() as ITicket[];
--- frontend/src/services/userApi.ts
+++ frontend/src/services/userApi.ts
@@ -1,21 +1,21 @@
 import { IUser } from "../interfaces/interface";
 
 
-export const getUsers = async (accesToken: string) => {
+export const getUsers = async (accessToken: string) => {
     const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Users`, {
-        headers: { Authorization: `Bearer ${accesToken}` }
+        headers: { Authorization: `Bearer ${accessToken}` }
     })
     return await response.json() as IUser[];
 }
 
-export const postUser = async (user: IUser, accesToken: string) => {
+export const postUser = async (user: IUser, accessToken: string) => {
     const userToJson = JSON.stringify(user);
     const response = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/Users`, {
         method: 'POST',
         body: userToJson,
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${accesToken}`
+            Authorization: `Bearer ${accessToken}`
         },
     })
     return await response.json() as IUser;
