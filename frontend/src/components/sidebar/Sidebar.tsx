48e2268e98ecc3b8428893c01835712bf2bdc7a1
Luc-Siat <lucsiat@gmail.com>
2023-03-26 12:19:26 +0200

    adding logo and 100vh on sidebar

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ export const Sidebar = () => {
   return (
     <aside className="sidebar">
       <menu className="sidebar__links">
+        <Link className="sidebar__logo"to=""><img src="logo.png" className="logo" alt="logo" /></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-house-chimney"></i></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-people-group"></i></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-gear"></i></Link>
