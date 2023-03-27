6c5be98512d630418db43bf1d49d3c2e4a21a05c
Luc-Siat <lucsiat@gmail.com>
2023-03-27 09:37:24 +0200

    connected tickets to ticket board

--- frontend/src/components/ticketboard/TicketBoard.tsx
+++ frontend/src/components/ticketboard/TicketBoard.tsx
@@ -2,18 +2,20 @@ import React, { FC, useContext } from 'react'
 import { TicketCard } from './ticketcard/TicketCard'
 import { TicketHeader } from './ticketheader/TicketHeader'
 import './ticketboard.css'
+import TicketProvider, { TicketsContext } from '../../context/TicketsProvider'
+import { TicketsContextType } from '../../types'
 
 type TicketBoardProps = {
   toggleAddBtn : () => void
 }
 
 export const TicketBoard :FC<TicketBoardProps> = ({ toggleAddBtn }) => {
-  // const Tickets = useContext()
+  const { tickets, fetchTickets } = useContext(TicketsContext) as TicketsContextType;
 
   return (
     <section className="ticket-board">
         <TicketHeader />
-        <TicketCard toggleAddBtn = {toggleAddBtn}/>
+        {tickets.map( ticket => <TicketCard toggleAddBtn = {toggleAddBtn} ticket={ticket} key={ticket.ticketId}/> )}
     </section>
   )
 }
