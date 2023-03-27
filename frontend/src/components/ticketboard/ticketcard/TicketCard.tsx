1ae5359484015412208961f26ea53fafe7c3518f
Luc-Siat <lucsiat@gmail.com>
2023-03-27 09:37:24 +0200

    connected tickets to ticket board

--- frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
+++ frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
@@ -1,20 +1,38 @@
 import React, { FC } from 'react'
+import { ITicket } from '../../../interfaces/interface'
 import './ticketcard.css'
 
 type TicketCardProps = {
-  toggleAddBtn : () => void
+  toggleAddBtn : () => void,
+  ticket: ITicket
 }
 
-export const TicketCard :FC<TicketCardProps> = ({ toggleAddBtn }) => {
+export const TicketCard :FC<TicketCardProps> = ({ toggleAddBtn, ticket }) => {
+
+
+  const handleUrgencySign = () => {
+    switch(ticket.urgency) { 
+      case 0: { 
+          return " fa-solid fa-triangle-exclamation"
+      } 
+      case 1: { 
+        return "color-yellow fa-solid fa-triangle-exclamation"
+      } 
+      case 2: { 
+        return "color-red fa-solid fa-triangle-exclamation"
+      } 
+   } 
+  }
+
   return (
     <ul className='ticket-board__grid fw-300 ticket-board__card-styling container'>
       <input onChange={() => toggleAddBtn()} className='ticket-board__checkbox' type="checkbox" />
-      <li className='ticket-board__id'>#1</li>
-      <li className='ticket-board__text'>Site is broken</li>
-      <li className='ticket-board__requester'>Donna</li>
-      <li className='ticket-board__requester-update'>Sunday 10am</li>
-      <li className='ticket-board__group'>Frontend</li>
-      <li className='ticket-board__assignee'>Lidia</li>
+      <li className='ticket-board__urgency'><i className={handleUrgencySign()}></i></li>
+      <li className='ticket-board__text'>{ticket.title}</li>
+      <li className='ticket-board__requester'>{ticket.creator.name ? ticket.creator.name : ticket.creator.email}</li>
+      <li className='ticket-board__requester-update'>{ticket.createdAt}</li>
+      <li className='ticket-board__group'>{ticket.status}</li>
+      <li className='ticket-board__assignee'>{ticket.assignedUser.name ? ticket.assignedUser.name : "-"}</li>
     </ul> 
   )
 }
