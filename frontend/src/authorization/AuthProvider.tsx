3a6935ba17b015aabfcfbe1c3e45e35d06230df8
cparn <caspar.rodoo@gmail.com>
2023-03-27 20:01:31 +0200

    start implementing auto post

--- frontend/src/authorization/AuthProvider.tsx
+++ frontend/src/authorization/AuthProvider.tsx
@@ -1,5 +1,6 @@
-import { Auth0Provider, AppState } from "@auth0/auth0-react";
+import { Auth0Provider, AppState, useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
+import { getUserByEmail, postUser } from "../services/userApi";
 
 interface AuthProviderProps {
     children: React.ReactNode
@@ -8,15 +9,28 @@ interface AuthProviderProps {
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
     const navigate = useNavigate();
+    const { user, getAccessTokenSilently } = useAuth0();
 
     const onRedirectCallback = (appState?: AppState) => {
+        const getOrPostUser = async () => {
+            const accessToken = await getAccessTokenSilently();
+            let dbUser = await getUserByEmail(user?.email, accessToken);
+            if (dbUser == null) {
+                dbUser = await postUser(accessToken);
+            }
+            if (dbUser == null) {
+                navigate('/403');
+            }
+            const {setDbUser}
+        }
+        getOrPostUser();
         navigate(appState?.returnTo || window.location.pathname);
     };
 
     if (!(import.meta.env.VITE_AUTH0_DOMAIN && import.meta.env.VITE_AUTH0_CLIENT_ID && import.meta.env.VITE_AUTH0_CALLBACK_URL && import.meta.env.VITE_AUTH0_AUDIENCE)) {
         return null;
     }
-    
+
     return (
         <Auth0Provider
             domain={import.meta.env.VITE_AUTH0_DOMAIN}
