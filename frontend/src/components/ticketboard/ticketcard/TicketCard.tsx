5114506ccd0d85997bb1adf4490d5e3db0dfbee9
Luc-Siat <lucsiat@gmail.com>
2023-03-27 14:38:15 +0200

    modify to a popup

--- frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
+++ frontend/src/components/ticketboard/ticketcard/TicketCard.tsx
@@ -8,56 +8,39 @@ type TicketCardProps = {
 }
 
 export const TicketCard :FC<TicketCardProps> = ({ toggleAddBtn, ticket }) => {
-  const [ticketSizeToggle, setTicketSizeToggle] = useState(false);
-
-
+  const [showPopUp, setShowPopUp] = useState(false);
+  
   const handleUrgencySign = () => {
     switch(ticket.urgency) { 
-      case 0: { 
-          return " fa-solid fa-triangle-exclamation"
-      } 
       case 1: { 
-        return "color-yellow fa-solid fa-triangle-exclamation"
+        return "color-yellow fa-solid fa-circle-exclamation";
       } 
       case 2: { 
-        return "color-red fa-solid fa-triangle-exclamation"
+        return "color-red fa-solid fa-circle-exclamation";
       } 
-   } 
+      default: {
+        return "";
+      }
+    }
   }
-
-  if (ticketSizeToggle) {
-    return (
-      <div onClick={() => setTicketSizeToggle(!ticketSizeToggle)} className="large-ticket-card ticket-board__card-styling border-bottom">
-        <ul className='ticket-board__grid fw-300 ticket-board__card-styling  container'>
-          <input onChange={() => toggleAddBtn()} className='ticket-board__checkbox' type="checkbox" />
-          <li className='ticket-board__urgency'><i className={handleUrgencySign()}></i></li>
-          <li className='ticket-board__text'>{ticket.title}</li>
-          <li className='ticket-board__requester'>{ticket.creator.name ? ticket.creator.name : ticket.creator.email}</li>
-          <li className='ticket-board__requester-update'>{ticket.createdAt}</li>
-          <li className='ticket-board__group'>{ticket.status}</li>
-          <li className='ticket-board__assignee'>{ticket.assignedUser.name ? ticket.assignedUser.name : "-"}</li>
-        </ul> 
-        <div className="large-ticket-card__content">
-          <p>{ticket.description}</p>
-          <button className='btn btn--blue'>Add ticket +</button>
-        </div>
-
-      </div>
-    )
-  }
-
 
 
 
   return (
-    <ul  onClick={() => setTicketSizeToggle(!ticketSizeToggle)}className='ticket-board__grid fw-300 ticket-board__card-styling container'>
+    <>
+    <ul onMouseEnter={() => setShowPopUp(!showPopUp)}  onMouseLeave={() => setShowPopUp(!showPopUp)} className='ticket-board__grid fw-300 ticket-board__card-styling container'>
       <input onChange={() => toggleAddBtn()} className='ticket-board__checkbox' type="checkbox" />
       <li className='ticket-board__urgency'><i className={handleUrgencySign()}></i></li>
       <li className='ticket-board__text'>{ticket.title}</li>
       <li className='ticket-board__requester'>{ticket.creator.name ? ticket.creator.name : ticket.creator.email}</li>
       <li className='ticket-board__requester-update'>{ticket.createdAt}</li>
       <li className='ticket-board__group'>{ticket.status}</li>
-      <li className='ticket-board__assignee'>{ticket.assignedUser.name ? ticket.assignedUser.name : "-"}</li>
+      <li className='ticket-board__assignee'>{ticket.assignedUser ? ticket.assignedUser.name : "-"}</li>
     </ul> 
+    { showPopUp && (<div className='popup-card'>
+      
+      Additional info: {ticket.description}
+    </div>)}
+    </>
   )
 }
