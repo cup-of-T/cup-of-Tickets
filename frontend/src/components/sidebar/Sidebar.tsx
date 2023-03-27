7b76c7ba9c8b96711fcca714c3997e82c37594e1
Luc-Siat <lucsiat@gmail.com>
2023-03-27 17:18:18 +0200

    moved ticket icon above gear

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -8,8 +8,8 @@ const Sidebar = () => {
         <Link className="sidebar__logo"to=""><img src="logoticket.png" className="logo" alt="logo" /></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-house-chimney"></i></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-people-group"></i></Link>
-        <Link className="sidebar__links-item"to=""><i className="fa-solid fa-gear"></i></Link>
         <Link className="sidebar__links-item"to=""><i className="fa-solid fa-ticket"></i></Link>
+        <Link className="sidebar__links-item"to=""><i className="fa-solid fa-gear"></i></Link>
       </menu>
     </aside>
   )
