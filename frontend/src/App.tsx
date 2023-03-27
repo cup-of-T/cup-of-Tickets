4ca959ce5dafb721bfbf3865324bfef22869fc0e
cparn <caspar.rodoo@gmail.com>
2023-03-27 15:56:50 +0200

    fix merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,35 +5,33 @@ import Header from './components/header/Header'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import Home from './pages/Home'
-import { IUserRequest } from './interfaces/interface'
-import ProtectedRoute from './components/ProtectedRoute'
+import { ITicket, IUser } from './interfaces/interface'
+import ProtectedRoute from './pages/ProtectedRoute'
 import Profile from './pages/Profile'
-import { getUserByEmail, getUsers, postUser } from './services/userApi'
+import { TicketsContext } from './context/TicketsProvider'
+import { TicketsContextType } from './types'
+import { getUsers } from './services/userApi'
 
 
 function App() {
   const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const { tickets, fetchTickets } = useContext(TicketsContext) as TicketsContextType;
+  const [users, setUsers] = useState<IUser[]>();
+
+  const getData = async () => {
+    const accessToken = await getAccessTokenSilently();
+    setUsers(await getUsers(accessToken));
+  }
 
   useEffect(() => {
-    const tryFetchUser = async () => {
-      const accessToken = await getAccessTokenSilently();
-      let dbUser = await getUserByEmail(user?.email!, accessToken);
-      if (dbUser === null) {
-        const newUser: IUserRequest = {
-          email: user!.email!,
-          imageUrl: user!.picture!
-         }
-        dbUser = await postUser(newUser, accessToken);
-      }
-      
-    }
-    if (isAuthenticated) {
-      tryFetchUser();
-    }
-  },);
+    getData();
+    fetchTickets();
+  }, [])
+
+  console.log(tickets);
 
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <main className="main center">
         <Routes>
