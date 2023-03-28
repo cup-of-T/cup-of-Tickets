fcadc7684e602c5d5d923e084ed4aa7acb90cd0a
LidiaOrfanu <lidia3991@yahoo.com>
2023-03-28 22:52:42 +0200

    test kv

--- frontend/src/authorization/AuthProvider.tsx
+++ frontend/src/authorization/AuthProvider.tsx
@@ -1,9 +1,21 @@
 import { Auth0Provider, AppState, useAuth0 } from "@auth0/auth0-react";
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+// import { DefaultAzureCredential, SecretClient } from "@azure/keyvault-secrets";
 import { useNavigate } from "react-router-dom";
 
 interface AuthProviderProps {
     children: React.ReactNode
 }
+
+const credential = new DefaultAzureCredential();
+const vaultName = "ticketVault";
+
+const url = `https://${vaultName}.vault.azure.net`;
+
+const client = new SecretClient(url, credential);
+const auth0Domain = await client.getSecret("vite-auth0-domain");
+const domain = auth0Domain.value!;
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
     const navigate = useNavigate();
@@ -12,13 +24,13 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
         navigate(appState?.returnTo || window.location.pathname);
     };
 
-    if (!(import.meta.env.VITE_AUTH0_DOMAIN && import.meta.env.VITE_AUTH0_CLIENT_ID && import.meta.env.VITE_AUTH0_CALLBACK_URL && import.meta.env.VITE_AUTH0_AUDIENCE)) {
+    if (!(domain && import.meta.env.VITE_AUTH0_CLIENT_ID && import.meta.env.VITE_AUTH0_CALLBACK_URL && import.meta.env.VITE_AUTH0_AUDIENCE)) {
         return null;
     }
 
     return (
         <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN}
+            domain={domain}
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
             authorizationParams={{
                 audience: import.meta.env.VITE_AUTH0_AUDIENCE,
