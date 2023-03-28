74b5999acecc8fa2805eb80de1a134b46fe3b901
cparn <caspar.rodoo@gmail.com>
2023-03-28 17:12:23 +0200

    fix loader

--- frontend/src/components/logincard/LoginCard.tsx
+++ frontend/src/components/logincard/LoginCard.tsx
@@ -3,14 +3,16 @@ import LoginButton from '../buttons/LoginButton'
 
 export const LoginCard = () => {
   return (
-    <>
+    <div className='login-card'>
+
       <div className="login-card__content">
         <div className="login-card__header">
-          <h1 className='login-card__header-title'>Welcome Back <br/> to Cup T  <img className="login-card__img" src="logoticket.png" alt="logo" /></h1>
+          <h1 className='login-card__header-title'>Welcome Back <br /> to Cup T  <img className="login-card__img" src="logoticket.png" alt="logo" /></h1>
         </div>
         <LoginButton />
       </div>
-     
-    </>
+    </div>
+
+
   )
 }
