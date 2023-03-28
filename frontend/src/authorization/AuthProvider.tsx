f22eb3b29b6e61733eb3aa8926ac5b663976d63b
LidiaOrfanu <lidia3991@yahoo.com>
2023-03-28 23:38:00 +0200

    get env from kv

--- frontend/src/authorization/AuthProvider.tsx
+++ frontend/src/authorization/AuthProvider.tsx
@@ -1,53 +1,54 @@
 import { Auth0Provider, AppState, useAuth0 } from "@auth0/auth0-react";
-import { DefaultAzureCredential } from "@azure/identity";
+import { InteractiveBrowserCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface AuthProviderProps {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
-const credential = new DefaultAzureCredential();
+
 const vaultName = "ticketVault";
 const url = `https://${vaultName}.vault.azure.net`;
-const client = new SecretClient(url, credential);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-  const navigate = useNavigate();
-  const [domain, setDomain] = useState<string | null>(null);
+    const navigate = useNavigate();
+    const [domain, setDomain] = useState<string | null>();
 
-  useEffect(() => {
-    const getAuth0Domain = async () => {
-      const auth0Domain = await client.getSecret("vite-auth0-domain");
-      setDomain(auth0Domain.value!);
+    useEffect(() => {
+        const getAuth0Domain = async () => {
+            const credential = new InteractiveBrowserCredential();
+            const client = new SecretClient(url, credential);
+            const auth0Domain = await client.getSecret("vite-auth0-domain");
+            setDomain(auth0Domain.value!);
+        };
+        getAuth0Domain();
+    }, []);
+
+    const onRedirectCallback = (appState?: AppState) => {
+        navigate(appState?.returnTo || window.location.pathname);
     };
-    getAuth0Domain();
-  }, []);
 
-  const onRedirectCallback = (appState?: AppState) => {
-    navigate(appState?.returnTo || window.location.pathname);
-  };
+    if (!domain || !import.meta.env.VITE_AUTH0_CLIENT_ID || !import.meta.env.VITE_AUTH0_CALLBACK_URL || !import.meta.env.VITE_AUTH0_AUDIENCE) {
+        return null;
+    }
 
-  if (!domain || !import.meta.env.VITE_AUTH0_CLIENT_ID || !import.meta.env.VITE_AUTH0_CALLBACK_URL || !import.meta.env.VITE_AUTH0_AUDIENCE) {
-    return null;
-  }
-
-  return (
-    <>
-      <Auth0Provider
-        domain={domain}
-        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-        authorizationParams={{
-          audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-          redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
-        }}
-        onRedirectCallback={onRedirectCallback}
-      >
-        {children}
-      </Auth0Provider>
-    </>
-  );
+    return (
+        <>
+            <Auth0Provider
+                domain={domain}
+                clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+                authorizationParams={{
+                    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+                    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+                }}
+                onRedirectCallback={onRedirectCallback}
+            >
+                {children}
+            </Auth0Provider>
+        </>
+    );
 }
 
 export default AuthProvider;
