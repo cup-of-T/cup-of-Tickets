89ab24d5d8022b06c2a50f794fb2c2818db464ed
cparn <caspar.rodoo@gmail.com>
2023-03-28 11:30:00 +0200

    add login page and reroute

---
+++ frontend/src/components/logincard/LoginCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginButton from '../buttons/LoginButton'
+
+export const LoginCard = () => {
+  return (
+    <>
+      <div className="login-card__content">
+        Welcome! Please login
+        <LoginButton />
+      </div>
+    </>
+  )
+}
