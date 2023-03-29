1f543c69044a2e3f647a3d1b91f4fae7cadf6f9e
idkwhoiam <donnasoujeh@gmail.com>
2023-03-29 09:20:15 +0200

    add textfield with formik

--- frontend/src/components/addticketform/TextField.tsx
+++ frontend/src/components/addticketform/TextField.tsx
@@ -1,21 +1,30 @@
+import { useField } from 'formik';
 import React from 'react'
 
 type TextFieldProps = {
   label: string,
   name: string,
+  type: string,
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string,
   value: string
 }
 
 export const TextField = (props: TextFieldProps) => {
+  const [field, meta] = useField(props);
+
   return (
-    <label>
-      {props.label}
-      <input
-        type="text"
-        {...props}
-      />
-    </label>
+    <>
+      <label htmlFor={props.name}>
+        {props.label}
+        <input
+          {...field}
+          {...props}
+        />
+      </label>
+      {meta.touched && meta.error ? (
+        <div>{meta.error}</div>) : null
+      }
+    </>
   )
 }
