de7b3062c5aff990534d70ba3dfa8d194bf316ab
Luc-Siat <lucsiat@gmail.com>
2023-03-30 15:09:56 +0200

    fix archive styling

--- frontend/src/pages/Archive.tsx
+++ frontend/src/pages/Archive.tsx
@@ -1,14 +1,15 @@
 import React, { useContext, useState } from 'react'
 import { StatsBar } from '../components/statsbar/StatsBar';
 import { TicketBoard } from '../components/ticketboard/TicketBoard';
+import { TicketsTable } from '../components/ticketboard/ticketstable/TicketsTable';
 import { TicketsContext } from '../context/TicketsProvider';
 import { TicketsContextType } from '../types';
 // import './pages.css';
 
 export const Archive = () => {
     const [addBtnToggle, setAddToggleBtn] = useState(false);
-    const [currentTicketIds, setCurrentTicketIds] = useState<number[]>([]);
     const { tickets } = useContext(TicketsContext) as TicketsContextType;
+    const [currentTicketIds, setCurrentTicketIds] = useState<number[]>([])
 
     const toggleAddBtn = (ticketId: number) => {
         if ( currentTicketIds.some(id => id == ticketId)) {
@@ -22,18 +23,13 @@ export const Archive = () => {
         setCurrentTicketIds([...currentTicketIds, ticketId]);
     }
 
-    const resetTicketsClaims = () => {
-        setCurrentTicketIds([]);
-        setAddToggleBtn(false);
-    }
-
-
     return (
-        <>
-            <StatsBar addBtnToggle={addBtnToggle} ticketIds={currentTicketIds} resetTicketsClaims={resetTicketsClaims}/>
-            <h3 className='container center page__title'>Archived tickets</h3>
-            <TicketBoard toggleAddBtn={toggleAddBtn} tickets={tickets?.filter(ticket => ticket.archived == true )} />
-        </>
+         <section className="ticket-board">
+            <TicketsTable title={'Available tickets'}
+                tickets={tickets.filter(ticket => ticket.archived == true)}
+                toggleAddBtn={toggleAddBtn}
+            />
+        </section>
     );
 };
 
