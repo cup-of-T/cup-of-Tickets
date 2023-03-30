ee3acdf96e67f1ad1daac3fafc372b51e8edfe36
Luc-Siat <lucsiat@gmail.com>
2023-03-30 14:53:05 +0200

    prop drill tickets from pages

--- frontend/src/components/addticketform/TextArea.tsx
+++ frontend/src/components/addticketform/TextArea.tsx
@@ -12,16 +12,17 @@ export const TextArea = ({ label, ...props }: TextAreaProps) => {
     const [field, meta] = useField(props);
 
     return (
-        <label className='text-area' htmlFor={label}>
+        <label className='ticket-form__label' htmlFor={label}>
             {label}
             <textarea
                 {...field}
                 {...props}
                 rows={props.rows}
+                className='grey--border'
             >
             </textarea>
             {meta.touched && meta.error ? (
-                <div className='text-area__error'>{meta.error}</div>) : null
+                <div className='ticket-form__label--error'>{meta.error}</div>) : null
             }
         </label>
     )
