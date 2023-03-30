d7acd40a66187add5d4a2b96741161dc746f35f5
cparn <caspar.rodoo@gmail.com>
2023-03-30 15:12:52 +0200

    add userhelper

---
+++ server/TicketsServer.Api/Services/UserHelper.cs
@@ -0,0 +1,26 @@
+using TicketsServer.Api.Models;
+
+namespace TicketsServer.Api.Services;
+
+internal static class UserHelper
+{
+    public static UserResponse UserToUserResponse(User user)
+    {
+        var responseList = user.Teams!.Select(t =>
+            new Team
+            {
+                TeamId = t.TeamId,
+                Name = t.Name,
+            })
+            .ToList();
+        return new UserResponse()
+        {
+            UserId = user.UserId,
+            Email = user.Email,
+            Name = user.Name,
+            Role = user.Role,
+            ImageUrl = user.ImageUrl,
+            Teams = responseList
+        };
+    }
+}
