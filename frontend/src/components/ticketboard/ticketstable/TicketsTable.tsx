4dffd10c4d2223d45bfbaae0a1c93789259ee759
cparn <caspar.rodoo@gmail.com>
2023-03-30 12:06:26 +0200

    refactor into components

---
+++ frontend/src/components/ticketboard/ticketstable/TicketsTable.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ITicket } from '../../../interfaces/interface'
+import { TicketCard } from '../ticketcard/TicketCard'
+import { TicketHeader } from '../ticketheader/TicketHeader'
+
+type TicketsTableProps = {
+    title: string,
+    tickets: ITicket[],
+    toggleAddBtn: (ticketId: number) => void
+}
+export const TicketsTable = ({ title, tickets, toggleAddBtn }: TicketsTableProps) => {
+    return (
+        <>
+            <h3>{title}</h3>
+            <TicketHeader />
+            {tickets.map(ticket => {
+                return (
+                    <div className="ticket-card">
+                        <TicketCard
+                            toggleAddBtn={toggleAddBtn}
+                            ticket={ticket}
+                            key={ticket.ticketId}
+                        />
+                    </div>
+                )
+            })}
+        </>
+    )
+}
