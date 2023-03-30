3efb134e39d01b4ea1082b413bef91048f888f48
cparn <caspar.rodoo@gmail.com>
2023-03-30 16:55:10 +0200

    start on teams

--- frontend/src/pages/Teams.tsx
+++ frontend/src/pages/Teams.tsx
@@ -1,11 +1,19 @@
+import { defaultAttributes } from "@dnd-kit/sortable/dist/hooks/defaults";
+import { Team } from "../components/team/Team";
+import { ITeam, IUser } from "../interfaces/interface";
+
 interface ITeamsProps {
+    teams: ITeam[],
+    user: IUser
 }
 
-const Teams = ({ }: ITeamsProps) => {
+const Teams = ({ teams, user }: ITeamsProps) => {
+    if (user == null) return;
+
+    const team = teams.find(t => t.users.find(u => u.userId == user.userId));
+
     return (
-        <>
-            Teams
-        </>
+        <Team team={team} />
     );
 };
 
