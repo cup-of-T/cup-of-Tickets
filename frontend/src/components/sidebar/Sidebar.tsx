34fb3cf9ba7e755b954be0641f863a93cd82ff28
Luc-Siat <lucsiat@gmail.com>
2023-03-30 11:54:02 +0200

    add back the img instead of urls

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <menu className="sidebar__links">
-        <Link className="sidebar__logo"to=""><img src="https://i.imgur.com/6BddD2q.png" className="logo" alt="logo" /></Link>
+        <Link className="sidebar__logo"to=""><img src="logoticket.png" className="logo" alt="logo" /></Link>
         <Link className="sidebar__links-item"to="/home"><i className="fa-solid fa-house-chimney"></i></Link>
         <Link className="sidebar__links-item" to="/dashboard"><i className="fa-solid fa-table-list"></i></Link>
         <Link className="sidebar__links-item"to="/kanban"><i className="fa-solid fa-ticket"></i></Link>
