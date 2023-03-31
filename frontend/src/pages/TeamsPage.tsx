ab9290039a0a83e5c37ae30d8fa36be677399eb0
idkwhoiam <donnasoujeh@gmail.com>
2023-03-31 17:08:04 +0200

    refactor and navbar

--- frontend/src/pages/TeamsPage.tsx
+++ frontend/src/pages/TeamsPage.tsx
@@ -1,6 +1,3 @@
-import { defaultAttributes } from "@dnd-kit/sortable/dist/hooks/defaults";
-import { divide } from "lodash";
-import Loader from "../components/loader/Loader";
 import { Team } from "../components/team/Team";
 import { ITeam, IUser } from "../interfaces/interface";
 import './pages.css'
@@ -11,12 +8,12 @@ interface ITeamsProps {
 }
 
 const TeamsPage = ({ teams, user }: ITeamsProps) => {
-
     const team = teams.find(t => t.users.find(u => u.userId == user.userId));
-    console.log(teams, team)
+
     if (team == null) {
         return (<>please join a team</>);
     }
+
     return (
         <div className="team-page">
             <Team team={team} />
