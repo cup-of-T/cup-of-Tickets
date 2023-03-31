ac316589621604121ba8a06229ca9bd47f41a3ba
Luc-Siat <lucsiat@gmail.com>
2023-03-31 18:52:29 +0200

    card is clickable

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -17,13 +17,14 @@ const Sidebar = () => {
         <Link className="sidebar__links-item" to="/dashboard"><i className="fa-solid fa-table-list"></i></Link>
         <Link className="sidebar__links-item" to="/kanban"><i className="fa-solid fa-ticket"></i></Link>
         <Link className="sidebar__links-item" to="/teams"><i className="fa-solid fa-people-group"></i></Link>
-        <Link className="sidebar__links-item" to="/archive"><i className="fa-solid fa-box-archive"></i></Link>
 
         <Link className="sidebar__links-item" to="/testing"><i className="fa-solid fa-flask"></i></Link>
         {(dbUser.role == "Manager" || dbUser.role == "Admin" ) && (
           <Link className="sidebar__links-item" to="/inprogress"><i className="fa-solid fa-person-digging"></i></Link>
-        )}
+          )}
 
+        <Link className="sidebar__links-item" to="/archive"><i className="fa-solid fa-box-archive"></i></Link>
+        
         <Link className="sidebar__links-item" to="/settings"><i className="fa-solid fa-gear"></i></Link>
       </menu >
     </aside >
