5e3f1b95d2b37efcf694f5e908d0f288f62bfe4d
idkwhoiam <donnasoujeh@gmail.com>
2023-03-31 14:26:28 +0200

    refactor code and implement alert

---
+++ frontend/src/components/ticketboard/ticketalert/TicketAlert.tsx
@@ -0,0 +1,10 @@
+import "./TicketAlert.css";
+
+export const TicketAlert = () => (
+    <div className="ticket-alert">
+        <div className="ticket-alert__information">
+            <i className="fa-solid fa-check"></i>
+            <p className="ticket-alert__paragraph">Successfully claimed!</p>
+        </div>
+    </div >
+);
--- frontend/src/components/ticketboard/ticketstable/TicketsTable.tsx
+++ frontend/src/components/ticketboard/ticketstable/TicketsTable.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ITicket } from '../../../interfaces/interface'
+import { TicketAlert } from '../ticketalert/TicketAlert'
 import { TicketCard } from '../ticketcard/TicketCard'
 import { TicketHeader } from '../ticketheader/TicketHeader'
 
@@ -7,11 +8,15 @@ type TicketsTableProps = {
     title: string,
     tickets: ITicket[],
     toggleAddBtn: (ticketId: number) => void
+    showAlert?: boolean
 }
-export const TicketsTable = ({ title, tickets, toggleAddBtn }: TicketsTableProps) => {
+
+export const TicketsTable = ({ title, tickets, toggleAddBtn, showAlert }: TicketsTableProps) => {
+
     return (
         <>
             <h3 className='page__title'>{title}</h3>
+            {showAlert && <TicketAlert />}
             <TicketHeader />
             {tickets.map(ticket => {
                 return (
