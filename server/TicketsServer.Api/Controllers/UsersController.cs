fd15a92d4beaad43f5c81474d20938dd3ab55747
cparn <caspar.rodoo@gmail.com>
2023-04-01 20:01:05 +0200

    change user get test

--- server/TicketsServer.Api/Controllers/UsersController.cs
+++ server/TicketsServer.Api/Controllers/UsersController.cs
@@ -23,7 +23,7 @@ public class UsersController : ControllerBase
 
     [HttpGet]
     [Authorize("User")]
-    public async Task<ActionResult<IEnumerable<UserResponse>>> GetUsers()
+    public async Task<ActionResult<List<UserResponse>>> GetUsers()
     {
         if (_context.Users == null)
         {
@@ -32,7 +32,7 @@ public class UsersController : ControllerBase
         var users = await _context.Users
             .Include(u => u.Teams)
             .ToListAsync();
-            
+
         return users.Select(user => UserHelper.UserToUserResponse(user)).ToList();
     }
 
--- server/TicketsServer.Tests/UsersHappyPath.cs
+++ server/TicketsServer.Tests/UsersHappyPath.cs
@@ -12,12 +12,21 @@ namespace TicketsServer.Tests;
 public class UsersHappyPath
 {
     private readonly DbContextOptions<DbContext> _options;
+    private readonly User MockedUser;
 
     public UsersHappyPath()
     {
         _options = new DbContextOptionsBuilder<DbContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
+
+        MockedUser = new User()
+        {
+            UserId = 1,
+            Email = "donna@kraken.se",
+            Name = "Donna Soujeh",
+            Role = "Developer",
+        };
     }
 
     [Fact]
@@ -26,12 +35,19 @@ public class UsersHappyPath
         var mockedFileService = new Mock<IFileService>();
         using (var context = new DbContext(_options))
         {
+            context.Users.Add(MockedUser);
+            await context.SaveChangesAsync();
+
             var controller = new UsersController(context, mockedFileService.Object);
 
             var response = await controller.GetUsers();
-            
+
             Assert.Equal(typeof(List<UserResponse>), response.Value?.GetType());
+            response.Value?.Count().Should().Be(1);
+            response.Value?[0].Name.Should().Be("Donna Soujeh");
         }
-        
     }
+
+
+
 }
