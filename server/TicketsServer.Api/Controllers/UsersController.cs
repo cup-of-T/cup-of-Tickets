d5eb8fa69c430ada6ba4f1ec6f36230c4d0e3e16
cparn <caspar.rodoo@gmail.com>
2023-04-02 20:12:16 +0200

    fix admin login

--- server/TicketsServer.Api/Controllers/UsersController.cs
+++ server/TicketsServer.Api/Controllers/UsersController.cs
@@ -62,7 +62,7 @@ public class UsersController : ControllerBase
 
         var teamExtractedFromEmail = email.Split('@')[1].Split('.')[0];
         var team = await _context.Teams.FirstOrDefaultAsync(t => t.Name.Contains(teamExtractedFromEmail));
-
+        
         roles.Sort();
         var role = roles[0];
 
@@ -75,7 +75,7 @@ public class UsersController : ControllerBase
             Name = textInfo.ToTitleCase(name),
             ImageUrl = picture,
             Role = role,
-            Teams = new List<Team>() { team }
+            Teams = team != null ? new List<Team>() { team } : new List<Team>()
         };
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
