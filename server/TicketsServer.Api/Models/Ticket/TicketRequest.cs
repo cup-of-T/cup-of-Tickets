c5a62b3a081cc0e05264a38c5943f6225d924a65
cparn <caspar.rodoo@gmail.com>
2023-04-02 12:46:51 +0200

    add team to ticket

--- server/TicketsServer.Api/Models/Ticket/TicketRequest.cs
+++ server/TicketsServer.Api/Models/Ticket/TicketRequest.cs
@@ -6,6 +6,7 @@ public class TicketRequest
     public required string Description { get; set; }
     public required string TimeEstimate { get; set; }
     public int Urgency { get; set; }
+    public required int TeamId { get; set; }
     public required List<string> CategoryNames { get; set; }
     public int UserId { get; set; }
 }
